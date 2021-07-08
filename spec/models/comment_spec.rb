e02db0d061dc4944cff8be6337067b9cd10f3536
Harefx <harefx.dev@gmail.com>
2021-07-09 01:52:08 +0700

    remove unnecessary spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 RSpec.describe Comment, type: :model do
   it { is_expected.to belong_to(:post) }
-  it { is_expected.to belong_to(:user) }
 
   it { is_expected.to validate_presence_of(:content) }
   it { is_expected.to validate_length_of(:content).is_at_least(3).is_at_most(255) }
