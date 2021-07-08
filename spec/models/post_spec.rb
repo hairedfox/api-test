203ecf0eaddb5b80249c28ba38680dcc18fcd331
Harefx <harefx.dev@gmail.com>
2021-07-08 23:38:36 +0700

    add spec for maximum length post title and body

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Post, type: :model do
   it { is_expected.to have_many(:comments) }
 
   it { is_expected.to validate_presence_of(:title) }
-  it { is_expected.to validate_length_of(:title).is_at_least(6) }
+  it { is_expected.to validate_length_of(:title).is_at_least(6).is_at_most(255) }
   it { is_expected.to validate_presence_of(:body) }
-  it { is_expected.to validate_length_of(:body).is_at_least(20) }
+  it { is_expected.to validate_length_of(:body).is_at_least(20).is_at_most(3_000) }
 end
